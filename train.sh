3078b5fdc36281bcb03236beecb0fe1db79167d3
tugajin <mamorukai2009@gmail.com>
2022-08-30 09:52:58 +0900

    学習用のshellを変更

--- train.sh
+++ train.sh
@@ -1,1 +1,7 @@
-python -m pydlshogi2.train /media/tugajin/HD-LDF-A/hcpe/hcpe/hcpe-20210815T091142Z-030/hcpe/elmo_teacher_shuffle-001 /media/tugajin/HD-LDF-A/hcpe/hcpe/hcpe-20210815T091142Z-030/hcpe/elmo_teacher_shuffle-007
+while :
+do
+  CHECKPOINTS_NAME=`ls -t checkpoints/ | head -n 1`
+  TRAIN_FILE=`ls -1 data/ | head -n 1`
+  python -m pydlshogi2.train data/$TRAIN_FILE floodgate_teacher-2013-01 -r checkpoints/$CHECKPOINTS_NAME -e 1
+  rm -f data/$TRAIN_FILE
+done
